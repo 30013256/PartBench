d05892688d9aed86c09b9dd24b4e11847525deb0
Jacques_Rockell <jacquesrockell@gmail.com>
2020-08-24 10:46:15 +1200

    committest

--- PartBench/PricePerformancePage.xaml.cs
+++ PartBench/PricePerformancePage.xaml.cs
@@ -3,12 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading.Tasks;
-using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.UI;
-using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -16,35 +12,22 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Windows.UI.Xaml.Shapes;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace PartBench
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class PricePerformancePage : Page
     {
-        public MainPage()
+        public PricePerformancePage()
         {
             this.InitializeComponent();
 
-            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
-            coreTitleBar.ExtendViewIntoTitleBar = true;
-
             // Set XAML element as a draggable region.
             Window.Current.SetTitleBar(AppTitleBar);
-     
-            //Changes Title bar colors
-            ApplicationView view = ApplicationView.GetForCurrentView();
-            ApplicationViewTitleBar titleBar = view.TitleBar;
-            titleBar.ForegroundColor = Windows.UI.Color.FromArgb(255, 0, 0, 255);
-            titleBar.ButtonBackgroundColor = Windows.UI.Color.FromArgb(0, 0, 0, 0);
-            titleBar.ButtonHoverBackgroundColor = Windows.UI.Color.FromArgb(20, 255, 255, 255);
-            titleBar.ButtonPressedBackgroundColor = Windows.UI.Color.FromArgb(50, 255, 255, 255);
-            titleBar.ButtonInactiveBackgroundColor = Windows.UI.Color.FromArgb(0, 255, 255, 255);
         }
 
         #region Nav Items
@@ -96,8 +79,6 @@ namespace PartBench
             margin.Left = 330;
             ContentContainer.Margin = margin;
         }
-
         #endregion
-
     }
 }
